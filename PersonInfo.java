7985fe48aeb521d54dab27c7602a7afd0efb1844
Jennifer-Nix-Yates <jennifer.nixyates@csuglobal.edu>
2025-01-13 13:24:13 -0700

    Create PersonInfo.java

---
+++ PersonInfo.java
@@ -0,0 +1,15 @@
+public class PersonInfo {
+    public static void main(String[] args) {
+        String FirstName = "Jen";
+        String LastName = "Nix-Yates";
+        String Addy = "123 Nunya Ln";
+        String City = "Colorado Springs";
+        String PostalCode = "12345";
+
+        System.out.println(FirstName);
+        System.out.println(LastName);
+        System.out.println(Addy);
+        System.out.println(City);
+        System.out.println(PostalCode);
+    }
+}
